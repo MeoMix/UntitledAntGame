ab15682c51a246531b52db2e8af3452c10f2a191
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2023-01-03 23:31:04 +0000

    Cut bugfix from next.config after moving code into WSL2

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // https://github.com/vercel/next.js/issues/36774
-  // https://github.com/microsoft/WSL/issues/4739
-  webpack: (config) => ({
-    ...config,
-    watchOptions: {
-      ...config.watchOptions,
-      poll: 1000,
-      aggregateTimeout: 200,
-    }
-  })
 }
 
 module.exports = nextConfig
