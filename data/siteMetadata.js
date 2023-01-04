def0e5c00f1cb00bf549a15d38801a98685a089f
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2023-01-04 10:04:09 -0800

    Update lockfile and basic fussing

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,12 +1,12 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Untitled Ant Game Blog',
+  author: 'Sean Anderson',
+  headerTitle: 'UntitledAntGameBlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://untitledantgame.com',
+  siteRepo: 'https://github.com/MeoMix/UntitledAntGame',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
