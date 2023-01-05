49b3599cd2b9e1e1a95366d82dbcd7fa1b23ff1e
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2023-01-05 02:23:08 +0000

    Fuss

--- pages/api/buttondown.ts
+++ pages/api/buttondown.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email } = req.body
   if (!email) {
-    return res.status(400).json({ error: 'Email is required' })
+    return res.status(400).json({ error: 'Email is required.' })
   }
 
   try {
